a9dd4a0c0ddc9f06222a0f670bde257edfa8d0de
lilei <289670502@qq.com>
2019-07-12 22:15:49 +0800

    opt

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(["exports", "./button"], factory);
+    define(["exports", "./button", "./icon"], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require("./button"));
+    factory(exports, require("./button"), require("./icon"));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.button);
+    factory(mod.exports, global.button, global.icon);
     global.index = mod.exports;
   }
-})(this, function (_exports, _button) {
+})(this, function (_exports, _button, _icon) {
   "use strict";
 
   var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
@@ -24,5 +24,12 @@
       return _button["default"];
     }
   });
+  Object.defineProperty(_exports, "Icon", {
+    enumerable: true,
+    get: function get() {
+      return _icon["default"];
+    }
+  });
   _button = _interopRequireDefault(_button);
+  _icon = _interopRequireDefault(_icon);
 });
